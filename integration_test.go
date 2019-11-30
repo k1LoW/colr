9a8381b3951618561dd5671c6d7404380698339e
k1LoW <k1lowxb@gmail.com>
2019-11-30 11:38:35 +0900

    Fix test

--- integration_test.go
+++ integration_test.go
@@ -17,18 +17,21 @@ func init() {
 
 func TestPaintAndErase(t *testing.T) {
 	var (
-		got []byte
-		err error
+		got  []byte
+		want string
+		err  error
 	)
 	if runtime.GOOS == "windows" {
-		got, err = exec.Command("cmd", "/c", `echo Hello | .\colr.exe He | .\colr.exe --erase`).Output()
+		got, err = exec.Command("cmd", "/c", `echo Hello| .\colr.exe He | .\colr.exe --erase`).Output()
+		want = "Hello\r\n"
 	} else {
-		got, err = exec.Command("bash", "-c", `echo Hello | ./colr He | ./colr --erase`).Output()
+		got, err = exec.Command("bash", "-c", `echo Hello| ./colr He | ./colr --erase`).Output()
+		want = "Hello\n"
 	}
 	if err != nil {
 		t.Fatal(err)
 	}
-	want := "Hello\n"
+
 	if want != string(got) {
 		t.Errorf("got = %#v ,want = %#v", string(got), want)
 	}
